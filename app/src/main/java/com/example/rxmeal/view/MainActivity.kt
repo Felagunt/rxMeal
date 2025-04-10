4cff2690fcd9035f5c332a834818a91f3f400f49
Mess <felagunt@live.ru>
2025-04-10 14:06:33 +0300

    first

--- app/src/main/java/com/example/rxmeal/view/MainActivity.kt
+++ app/src/main/java/com/example/rxmeal/view/MainActivity.kt
@@ -32,11 +32,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import coil3.compose.AsyncImage
 import coil3.compose.SubcomposeAsyncImage
 import com.example.rxmeal.view.ui.theme.RxMealTheme
 import com.example.rxmeal.viewModel.MealViewModel
@@ -79,7 +76,7 @@ fun MainContent(
 ) {
 
     Scaffold(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxSize(),
         topBar = {
             TextField(
